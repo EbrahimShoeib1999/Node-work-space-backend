7e43653135858627c40c4984eab61de94e35bbee
Rashad <rashadatef2@gmail.com>
2025-01-14 08:38:00 +0200

    chore : added get order by id endpoint

--- features/order/router/order-router.js
+++ features/order/router/order-router.js
@@ -4,12 +4,11 @@ const OrderController = require("../controller/order-controller");
 
 // Order routes
 router.post("/", OrderController.createOrder); // Create a new order
-router.post("/:id/pay", OrderController.payOrder); // Pay for an order
-router.get("/client/:clientId", OrderController.getOrdersByClientId); // Get orders by client ID
 router.get("/", OrderController.getAllOrders); // Get all orders
 router.get("/:id", OrderController.getOrderById); // Get all orders
-
 router.delete("/:id", OrderController.deleteOrder); // Delete a specific order
+router.post("/:id/pay", OrderController.payOrder); // Pay for an order
+router.get("/client/:clientId", OrderController.getOrdersByClientId); // Get orders by client ID
 
 // Order item routes
 router.post("/item", OrderController.addOrderItem); // Add item to a specific order
