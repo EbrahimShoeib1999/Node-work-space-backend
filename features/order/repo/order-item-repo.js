2fdac76d2b403c502809e91e69a8c1aef0c509e2
Rashad <rashadatef2@gmail.com>
2025-02-06 06:21:13 +0200

    fix : fixed order item

--- features/order/repo/order-item-repo.js
+++ features/order/repo/order-item-repo.js
@@ -1,4 +1,7 @@
 const OrderItem  = require("../models/order-item");
+const Order = require("../models/order");
+const Client = require("../../client/models/client");
+const Inventory = require("../../inventory/models/inventory")
 
 class OrderItemRepository {
   async addOrderItem(data) {
@@ -9,20 +12,36 @@ class OrderItemRepository {
     return await OrderItem.findByPk(orderItemId);
   }
 
+
   async findPreparingOrderItems() {
     try {
-      // Find all order items where the status is 'preparing'
-      // Return the found items
       return await OrderItem.findAll({
-        where: {
-          status: 'preparing'
-        }
+        where: { status: 'preparing' },
+        include: [
+          {
+            model: Order, // Ensure correct path to the Order model
+            as: "order",
+            include: [
+              {
+                model: Client, // Ensure correct path to the Client model
+                as: "client",
+                attributes: ["id", "name",], // Select only necessary fields
+              },
+            ],
+          },
+          {
+            model: Inventory, // Ensure correct path to the Inventory model
+            as: "inventoryItem",
+            attributes: ["id", "name",], // Select only necessary fields
+          },
+        ],
       });
     } catch (error) {
-      console.error('Error finding preparing order items:', error.message);
-      throw new Error('Error finding preparing order items: ' + error.message);
+      console.error("Error finding preparing order items:", error.message);
+      throw new Error("Error finding preparing order items: " + error.message);
     }
   }
+
 
 
   async markOrderAsReady(orderId) {
