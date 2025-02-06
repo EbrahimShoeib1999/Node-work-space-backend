4d1fadedca13778599ed9cf305e8f35b2471da88
Rashad <rashadatef2@gmail.com>
2025-02-06 06:21:32 +0200

    fix : fixed order item

--- features/order/repo/order-item-repo.js
+++ features/order/repo/order-item-repo.js
@@ -45,8 +45,6 @@ class OrderItemRepository {
 
 
   async markOrderAsReady(orderId) {
-
-    console.log('markOrderAsRead', orderId);
     const orderItem = await OrderItem.findByPk(orderId)
 
     if(!orderItem) {
